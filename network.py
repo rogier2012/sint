4f0cbd1c3e417a22d00c151314c026aef97655d9
Rogier <r.monshouwer@student.utwente.nl>
2016-11-30 00:06:49 +0100

    initial network

--- network.py
+++ network.py
@@ -7,4 +7,12 @@ from subprocess import call
 
 
 res = call(["cat", "/proc/net/wireless"])
-print(res)
+for x in res:
+    print(x)
+quality = 0
+if (quality <= 0):
+    dBm = -100
+elif (quality >= 100):
+    dBm = -50
+else:
+    dBm = (quality / 2) - 100
