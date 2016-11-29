4e6b8785498bcc9e1c24d8c668642e239163d9d0
Rogier <r.monshouwer@student.utwente.nl>
2016-11-30 00:24:31 +0100

    initial network

--- network.py
+++ network.py
@@ -6,22 +6,24 @@ import matplotlib as plt
 from subprocess import *
 
 
-res = Popen(["cat", "/proc/net/wireless"],stdout=PIPE )
-corr = ""
-line = res.stdout.readline()
-while line != '':
-        # print(line)
-        if "wlan0" in line:
-            corr = line
-        line = res.stdout.readline()
-# print(corr[18:-30])
+result = 0.0
+for i in range(100):
+    res = Popen(["cat", "/proc/net/wireless"],stdout=PIPE )
+    corr = ""
+    line = res.stdout.readline()
+    while line != '':
+            # print(line)
+            if "wlan0" in line:
+                corr = line
+            line = res.stdout.readline()
+    # print(corr[18:-30])
 
-quality = int(corr[20:-53])
-if (quality <= 0):
-    dBm = -100
-elif (quality >= 100):
-    dBm = -50
-else:
-    dBm = (quality / 2) - 100
+    quality = int(corr[20:-53])
+    if (quality <= 0):
+        dBm = -100
+    elif (quality >= 100):
+        dBm = -50
+    else:
+        dBm = (quality / 2) - 100
 
-print(dBm)
+    result = float(result) + (float(dBm) / 100)
